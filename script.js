23718a60cdfd02c53f7f8ddb372d4b285b9840f5
Van4ello48 <samokhin-vanya@mail.ru>
2022-09-20 19:15:53 +0300

    ДЗ 2 урока по JS

--- script.js
+++ script.js
@@ -5,20 +5,48 @@ const goods = [
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
-    ];
+];
 
-const renderGoodsItem = ({title = "", price = 0}) => `
-    <div class="goods-item">
-        <div class="goods-item-img">
-        </div><h3>${title}</h3>
-        <p>Цена товара: $${price}</p>
-        <button class="addToCart">Добавить</button>
-    </div>
-    `;
+class GoodsItem {
+    constructor ({title = "", price = 0}) {
+        this.title = title;
+        this.price = price;
+    }
 
-const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => renderGoodsItem(item)).join("");
-    document.querySelector('.goods-list').innerHTML = goodsList;
-}
+    render() {
+        return `
+        <div class="goods-item">
+            <div class="goods-item-img">
+            </div><h3>${this.title}</h3>
+            <p>Цена товара: $${this.price}</p>
+            <button class="addToCart">Добавить</button>
+        </div>
+        `
+    }
+};
 
-renderGoodsList(goods);
+class GoodsList {
+    list = [];
+    fetchGoods() {
+        this.list = goods;
+    }
+
+    render() {
+        let resultList = this.list.map(item => {
+            const goodsItem = new GoodsItem(item);
+            return goodsItem.render();
+        }).join("");
+
+        document.querySelector('.goods-list').innerHTML = resultList;
+    };
+
+    totalPrice() {
+        console.log(this.list.reduce((sum, product) => sum + product.price, 0));
+    }
+};
+
+const goodsList = new GoodsList;
+
+goodsList.fetchGoods();
+goodsList.render();
+goodsList.totalPrice();
