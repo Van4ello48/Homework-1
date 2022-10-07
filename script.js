85f0ec4aeb2f0a713ba2e95c30a8e5dd188b9ba2
Van4ello48 <samokhin-vanya@mail.ru>
2022-10-07 19:53:34 +0300

    ДЗ 6 урока по JavaScript

--- script.js
+++ script.js
@@ -9,34 +9,76 @@ function service(url) {
     return fetch(url).then((res) => res.json())
 }
 
-const app = new Vue({
-    el: '#root',
-    data: {
-        goods: [],
-        search: "",
-        isVisibleCart: false,
-    },
-    mounted() {
-        setTimeout(() => {
-            service(url).then((data) => {
-                this.goods = data;
-            })
-        }, 2000)
-    },
-    computed: {
-        totalPrice() {
-            return this.goods.reduce((sum, product) => sum + product.price, 0);
+function init() {
+    Vue.component('goods_item', {
+        props: [
+            'item'
+        ],
+        template: `
+            <div class="goods-item">
+                <div class="goods-item-img"></div>
+                <h3>{{item.product_name}}</h3>
+                <p>Цена товара: \${{item.price}}</p>
+                <button class="addToCart">Добавить</button>
+            </div>
+        `
+    });
+
+    Vue.component('cart_button', {
+        template: `
+            <button class="cart-button" type="button" @click="$emit('click')">
+                <slot></slot>
+            </button>
+        `
+    });
+
+    Vue.component('fixed_cart', {
+        props: [
+            'isVisibleCart'
+        ],
+        template: `
+            <div class="fixed-cart-background">
+                <div class="fixed-cart">
+                    <div class="fixed-cart-header">
+                        <h1 class="fixed-cart-name">basket card</h1>
+                        <div class="fixed-cart-close" @click="openCart">x</div>
+                    </div>
+                </div>
+            </div>
+        `
+    });
+    
+
+    const app = new Vue({
+        el: '#root',
+        data: {
+            goods: [],
+            search: "",
+            isVisibleCart: false,
         },
-        filteredGoods() {
-            return this.goods.filter((item) => {
-                const regExp = new RegExp(this.search, 'gi');
-                return regExp.test(item.product_name)
-            })
+        mounted() {
+            setTimeout(() => {
+                service(url).then((data) => {
+                    this.goods = data;
+                })
+            }, 2000)
+        },
+        computed: {
+            totalPrice() {
+                return this.goods.reduce((sum, product) => sum + product.price, 0);
+            },
+            filteredGoods() {
+                return this.goods.filter((item) => {
+                    const regExp = new RegExp(this.search, 'gi');
+                    return regExp.test(item.product_name)
+                })
+            }
+        },
+        methods: {
+            openCart() {
+                this.isVisibleCart = !this.isVisibleCart;
+            },
         }
-    },
-    methods: {
-        openCart() {
-            this.isVisibleCart = !this.isVisibleCart;
-        },
-    }
-})
+    })
+}
+init();
